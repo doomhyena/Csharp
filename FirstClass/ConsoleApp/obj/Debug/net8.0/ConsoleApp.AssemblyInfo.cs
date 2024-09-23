083fe9104af3f3b5feb4f03ce60148351c7033a5
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-09-23 09:20:16 +0200

    aha
    aha

--- FirstClass/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ FirstClass/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1e3faab662a9ba00529e483cf9fc774e2749a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de33d0b774a3c96f60462d86622fcd8e13944fab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

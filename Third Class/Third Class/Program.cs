792efb50470ff664a5804b6326d6abda1ac7e176
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-02 14:10:24 +0200

    Harmadik óra
    Harmadik óra eleje

---
+++ Third Class/Third Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Glassic(int a, int b) { 
+            
+            
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

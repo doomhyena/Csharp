20684041abb4b957f261c5a2f4ff120e9a0114a1
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-10-09 21:00:21 +0200

    hmm

--- Fourth Class/Third Class/Program.cs
+++ Fourth Class/Third Class/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fourth_Class
 {
@@ -11,6 +7,46 @@ namespace Fourth_Class
         static Random rnd = new Random();
         static void Main(string[] args)
         {
+        // Console.WriteLine("Hello World");
+        Console.Clear();
+
+        Console.WindowWidth = 150;
+        Console.WindowHeight = 50;
+        Console.ForgroundColor = ConsoleColor.BarkBlue;
+        Console.BackroundColor =  ConsoleColor.Black;
+        Coonsole.WriteLine($"WIdth:"  + Console.WindowWidth);
+        Coonsole.WriteLine($"Height:"  + Console.WindowHeight);
+
+        for (int y = 0; y < Console.WindowHeight; y++) {
+            for (int x = 0; x < Console.WindowWidth; x++) {
+                Console.SetCursorPosition(x, y);
+                Console.Write('');
+            }
+        }
+
+        int x, y;
+
+        do 
+        {
+            x = rnd.Next(Console.WindowWidth);
+            y = rnd.Next(Console.WindowWidth);
+                Console.Write('');
+
+        } while (true);
+        
+        // ---------------------------------------------------------------
+
+
+        int x, y;
+
+        COnsoleColor[] colors = new COnsoleColor[]  {
+            ConsoleColor.Red,ConsoleColor.Green,ConsoleColor.Blue,ConsoleColor.Magenta,ConsoleColor.Cyan,
+            ConsoleColor.Yellow,ConsoleColor.Gray,ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,Console025
+        }
+
+
+
+        Console.ReadKey();
         }
     }
 } 

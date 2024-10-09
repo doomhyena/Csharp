799425bacaa899778c222d87d9694f5a75b57ea5
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-10-09 13:47:00 +0200

    Eheaha
    Eheaha

---
+++ Sample/Third Class/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourth_Class
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+        }
+    }
+} 
